4fd1e0d55a691666c00808a9de2ddfd2f3bf5419
magnus.leray <magnus.leray@uc.se>
2017-12-18 14:16:39 +0100

    pageModel

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 "# Seleniumtest" 
+
+
+Select new select value för listor mm
+
+popup fönster 
+
+ArrayList<String> windows = new arrayList<String>(driver.getWindowHandles());
+driver.switchTo.window(windows,get(0));
--- src/test/java/se/udemy/SeleniumAndCumber/SeleniumUmdey/AppTest.java
+++ src/test/java/se/udemy/SeleniumAndCumber/SeleniumUmdey/AppTest.java
@@ -1,5 +1,9 @@
 package se.udemy.SeleniumAndCumber.SeleniumUmdey;
 
+
+
+import org.openqa.selenium.WebDriver;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,32 +11,20 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+
+public class AppTest   
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+WebDriver driver;	
+	
+public void setlogin(){
+	
+}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public AppTest(WebDriver driver){
+	this.driver = driver;
+	
+}
+	
+	
 }
